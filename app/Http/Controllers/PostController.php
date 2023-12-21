f067cdcecc7bf07570457f785e9d1029f3915e30
yuya0022 <yuuya.matumotojyou@icloud.com>
2023-12-22 04:22:43 +0900

    Add ブログ投稿詳細画面の作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,6 +11,11 @@ class PostController extends Controller
     {
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
     }
+    
+    public function show(Post $post)
+    {
+        return view('posts.show')->with(['post' => $post]);
+    }
 }
 
 ?>
