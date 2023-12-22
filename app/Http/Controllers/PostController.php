1574e745fb99f61ac3fa261a6acdbffa88fdf3a7
yuya0022 <yuuya.matumotojyou@icloud.com>
2023-12-23 05:28:54 +0900

    ブログ投稿作成画面の保存処理実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,6 +21,13 @@ class PostController extends Controller
     {
         return view('posts.create');
     }
+    
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
 }
 
 ?>
