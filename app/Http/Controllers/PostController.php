676a42329acb21f9d28760c45932f645a9694047
yuya0022 <yuuya.matumotojyou@icloud.com>
2023-12-23 23:49:11 +0900

    Add ブログ投稿作成画面の保存時入力チェック処理実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 {
@@ -22,7 +23,7 @@ class PostController extends Controller
         return view('posts.create');
     }
     
-    public function store(Request $request, Post $post)
+    public function store(PostRequest $request, Post $post)
     {
         $input = $request['post'];
         $post->fill($input)->save();
